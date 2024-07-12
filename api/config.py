c04eb357f64cc15489028da91f91ff3f1fc42f2c
ArielMAJ <ariel.maj@hotmail.com>
2024-07-12 09:06:41 -0300

    fix: change POSTGRES_DB_NAME environment variable in config.py

--- api/config.py
+++ api/config.py
@@ -13,11 +13,11 @@ class DatabaseConfig:
     POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "postgres")
     POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
     POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
-    POSTGRES_DB_NAME = os.getenv("POSTGRES_DB_NAME", "postgres")
+    POSTGRES_DATABASE = os.getenv("POSTGRES_DATABASE", "postgres")
     POSTGRES_URI = (
         f"{DATABASE_CONNECTOR}:"
         f"//{POSTGRES_USER}:{POSTGRES_PASSWORD}"
-        f"@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB_NAME}"
+        f"@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DATABASE}"
     )
     POSTGRES_ECHO = os.getenv("POSTGRES_ECHO", "false").lower() == "true"
     POOL_PRE_PING = os.getenv("POOL_PRE_PING", True)
